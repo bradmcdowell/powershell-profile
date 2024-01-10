ba7c256229638eac02ccbba6571b6bf53927d76f
Brad McDowell <brad@mcdowell.com.au>
2024-01-10 22:35:41 +1100

    Changed scripts

---
+++ RunAsAdmin.ps1
@@ -0,0 +1,38 @@
+# OMP Install
+#
+winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
+
+# Font Install
+# Get all installed font families
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+$fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
+
+# Check if CaskaydiaCove NF is installed
+if ($fontFamilies -notcontains "CaskaydiaCove NF") {
+    
+    # Download and install CaskaydiaCove NF
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
+
+    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
+    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+            # Install font
+            $destination.CopyHere($_.FullName, 0x10)
+        }
+    }
+
+    # Clean up
+    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
+    Remove-Item -Path ".\CascadiaCode.zip" -Force
+}
+
+
+# Choco install
+#
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Terminal Icons Install
+#
+Install-Module -Name Terminal-Icons -Repository PSGallery -Force
