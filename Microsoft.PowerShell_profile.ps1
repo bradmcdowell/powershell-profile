255b480c691bb26c93cf8dcd1f71a0a5120764b1
Brad McDowell <brad@mcdowell.com.au>
2024-01-11 10:34:06 +1100

    Updated Script

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -200,10 +200,10 @@ function pgrep($name) {
 # Be aware that if you are missing these lines from your profile, tab completion
 # for `choco` will not function.
 # See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-    Import-Module "$ChocolateyProfile"
-}
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#     Import-Module "$ChocolateyProfile"
+# }
 
 # Invoke-Expression (& { (zoxide init powershell | Out-String) })
 
