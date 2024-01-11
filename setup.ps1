de6743f0bc8077888f591a7808880776f2aef82c
Brad McDowell <brad@mcdowell.com.au>
2024-01-11 11:12:41 +1100

    Update

--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,73 @@
 winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
 
+# Font Install
+# Get all installed font families
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+$fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Families
+
+# Check if CaskaydiaCove NF is installed
+if ($fontFamilies -notcontains "Cascadia Code") {
+    
+    # Download and install CaskaydiaCove NF
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
+
+    Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
+    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+            # Install font
+            $destination.CopyHere($_.FullName, 0x10)
+        }
+    }
+
+    # Clean up
+    Remove-Item -Path ".\CascadiaCode" -Recurse -Force
+    Remove-Item -Path ".\CascadiaCode.zip" -Force
+}
+
+# Check if CaskaydiaCove NF is installed
+if ($fontFamilies -notcontains "Hack Nerd Font") {
+    
+    # Download and install CaskaydiaCove NF
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Hack.zip", ".\HackNF.zip")
+
+    Expand-Archive -Path ".\HackNF.zip" -DestinationPath ".\HackNF" -Force
+    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem -Path ".\HackNF" -Recurse -Filter "*.ttf" | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+            # Install font
+            $destination.CopyHere($_.FullName, 0x10)
+        }
+    }
+
+    # Clean up
+    Remove-Item -Path ".\HackNF" -Recurse -Force
+    Remove-Item -Path ".\HackNF.zip" -Force
+}
+
+# Check if FiraCode is installed
+if ($fontFamilies -notcontains "FiraCode Nerd Font") {
+    
+    # Download and install CaskaydiaCove NF
+    $webClient = New-Object System.Net.WebClient
+    $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/FiraCode.zip", ".\FiraCode.zip")
+
+    Expand-Archive -Path ".\FiraCode.zip" -DestinationPath ".\FiraCode" -Force
+    $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
+    Get-ChildItem -Path ".\FiraCode" -Recurse -Filter "*.ttf" | ForEach-Object {
+        If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
+            # Install font
+            $destination.CopyHere($_.FullName, 0x10)
+        }
+    }
+
+    # Clean up
+    Remove-Item -Path ".\FiraCode" -Recurse -Force
+    Remove-Item -Path ".\FiraCode.zip" -Force
+}
+
 
 #If the file does not exist, create it.
 if (!(Test-Path -Path $PROFILE -PathType Leaf)) {
