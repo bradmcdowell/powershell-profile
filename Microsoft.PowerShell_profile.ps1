d70c1c0920f290fd7ea618b27d8416772486b72a
Brad McDowell <brad@mcdowell.com.au>
2026-09-16 12:07:38 +1000

    2.2.1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-$MSProfileVersion = "2026.09.02.1"
+$MSProfileVersion = "2026.09.16.1"
 Write-Output "Profile Version: $MSProfileVersion"
 
 # ==============================================================================
