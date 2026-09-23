c1c724e438ee62e89e63c43b667839c72fbf015e
Brad McDowell <brad@mcdowell.com.au>
2026-09-23 20:58:50 +1000

    2.6

--- RunAsAdmin.ps1
+++ RunAsAdmin.ps1
@@ -1,13 +1,1 @@
-# OMP Install
-#
-#winget install -e --accept-source-agreements --accept-package-agreements JanDeDobbeleer.OhMyPosh
-
-# Choco install
-#
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-
-# Terminal Icons Install
-#
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
-
-Set-ExecutionPolicy RemoteSigned
